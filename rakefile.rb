2cd03040dd976cb6cef51c07b704e5841052daab
Josh Arnold <rncodepoet@gmail.com>
2012-06-26 13:43:39 -0500

    Adds nuspec and updates the rakefile to the latest and greatest

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,18 @@
-COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
+COMPILE_TARGET = ENV['config'].nil? ? "Debug" : ENV['config'] # Keep this in sync w/ VS settings since Mono is case-sensitive
+CLR_TOOLS_VERSION = "v4.0.30319"
+
+buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
+
+if( ! buildsupportfiles.any? )
+  # no buildsupport, let's go get it for them.
+  sh 'git submodule update --init' unless buildsupportfiles.any?
+  buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
+end
+
+# nope, we still don't have buildsupport. Something went wrong.
+raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
+
+buildsupportfiles.each { |ext| load ext }
 
 include FileTest
 require 'albacore'
@@ -6,32 +20,47 @@ load "VERSION.txt"
 
 RESULTS_DIR = "results"
 PRODUCT = "FubuMVC.ServerSentEvents"
-COPYRIGHT = 'Copyright 2012 FubuMVC.ServerSentEvents. All rights reserved.';
+COPYRIGHT = 'Copyright 2012 Jeremy D. Miller, et al. All rights reserved.';
 COMMON_ASSEMBLY_INFO = 'src/CommonAssemblyInfo.cs';
-CLR_TOOLS_VERSION = "v4.0.30319"
-
-buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
-buildsupportfiles.each { |ext| load ext }
-
+BUILD_DIR = File.expand_path("build")
+ARTIFACTS = File.expand_path("artifacts")
 
 tc_build_number = ENV["BUILD_NUMBER"]
 build_revision = tc_build_number || Time.new.strftime('5%H%M')
-build_number = "#{BUILD_VERSION}.#{build_revision}"
-BUILD_NUMBER = build_number 
+BUILD_NUMBER = "#{BUILD_VERSION}.#{build_revision}"
 
-
-props = { :stage => File.expand_path("build"), :artifacts => File.expand_path("artifacts") }
+props = { :stage => BUILD_DIR, :artifacts => ARTIFACTS }
 
 desc "**Default**, compiles and runs tests"
 task :default => [:compile, :unit_test]
 
 desc "Target used for the CI server"
-task :ci => [:update_all_dependencies, :default, :history, :publish]
+task :ci => [:update_all_dependencies, :default, :history, :package]
+
+desc "Update the version information for the build"
+assemblyinfo :version do |asm|
+  asm_version = BUILD_VERSION + ".0"
+  
+  begin
+    commit = `git log -1 --pretty=format:%H`
+  rescue
+    commit = "git unavailable"
+  end
+  puts "##teamcity[buildNumber '#{BUILD_NUMBER}']" unless tc_build_number.nil?
+  puts "Version: #{BUILD_NUMBER}" if tc_build_number.nil?
+  asm.trademark = commit
+  asm.product_name = PRODUCT
+  asm.description = BUILD_NUMBER
+  asm.version = asm_version
+  asm.file_version = BUILD_NUMBER
+  asm.custom_attributes :AssemblyInformationalVersion => asm_version
+  asm.copyright = COPYRIGHT
+  asm.output_file = COMMON_ASSEMBLY_INFO
+end
 
 desc "Prepares the working directory for a new build"
-task :clean do
-	#TODO: do any other tasks required to clean/prepare the working directory
+task :clean => [:update_buildsupport] do
+	
 	FileUtils.rm_rf props[:stage]
     # work around nasty latency issue where folder still exists for a short while after it is removed
     waitfor { !exists?(props[:stage]) }
@@ -39,28 +68,6 @@ task :clean do
     
 	Dir.mkdir props[:artifacts] unless exists?(props[:artifacts])
 end
-
-desc "Update the version information for the build"
-assemblyinfo :version do |asm|
-  asm_version = build_number
-  
-  begin
-    commit = `git log -1 --pretty=format:%H`
-  rescue
-    commit = "git unavailable"
-  end
-  puts "##teamcity[buildNumber '#{build_number}']" unless tc_build_number.nil?
-  puts "Version: #{build_number}" if tc_build_number.nil?
-  asm.trademark = commit
-  asm.product_name = PRODUCT
-  asm.description = build_number
-  asm.version = asm_version
-  asm.file_version = build_number
-  asm.custom_attributes :AssemblyInformationalVersion => asm_version
-  asm.copyright = COPYRIGHT
-  asm.output_file = COMMON_ASSEMBLY_INFO
-end
-
 
 def waitfor(&block)
   checks = 0
@@ -72,25 +79,34 @@ def waitfor(&block)
 end
 
 desc "Compiles the app"
-msbuild :compile => [:restore_if_missing, :version] do |msb|
-	msb.command = File.join(ENV['windir'], 'Microsoft.NET', 'Framework', CLR_TOOLS_VERSION, 'MSBuild.exe')
-	msb.properties :configuration => COMPILE_TARGET
-	msb.solution = "src/FubuMVC.ServerSentEvents.sln"
-    msb.targets :Rebuild
-    msb.log_level = :verbose
+task :compile => [:restore_if_missing, :clean, :version] do
+  MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'src/FubuMVC.ServerSentEvents.sln', :clrversion => CLR_TOOLS_VERSION
+
+  target = COMPILE_TARGET.downcase
 end
 
-desc "Run unit tests"
-task :unit_test do 
+def copyOutputFiles(fromDir, filePattern, outDir)
+  Dir.glob(File.join(fromDir, filePattern)){|file| 		
+	copy(file, outDir, :preserve => true) if File.file?(file)
+  } 
+end
+
+desc "Runs unit tests"
+task :test => [:unit_test]
+
+desc "Runs unit tests"
+task :unit_test => :compile do
   runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'src', :platform => 'x86'
-  tests = Array.new
-  file = File.new("TESTS.txt", "r")
-  assemblies = file.readlines()
-  assemblies.each do |a|
-	test = a.gsub("\r\n", "").gsub("\n", "")
-	tests.push(test)
-  end
-  file.close
-  
-  runner.executeTests tests
+  runner.executeTests ['FubuMVC.ServerSentEvents.Testing']
+end
+
+def self.bottles(args)
+  bottles = Platform.runtime(Nuget.tool("Bottles", "BottleRunner.exe"))
+  sh "#{bottles} #{args}"
+end
+
+
+def self.fubu(args)
+  fubu = Platform.runtime("src/fubu/bin/#{COMPILE_TARGET}/fubu.exe")
+  sh "#{fubu} #{args}"
 end
