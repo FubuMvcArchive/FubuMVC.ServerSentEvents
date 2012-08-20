ac897fa2f318bfca30595100a8fc557ebaebc34b
Jeremy Miller <jeremydmiller@yahoo.com>
2012-08-20 15:12:19 -0500

    rev'ed to a fixed FubuMVC for the WrappingBehavior fix that blew up StructureMap

--- src/FubuMVC.ServerSentEvents.Testing/ServerSentEventExtensionIntegratedTester.cs
+++ src/FubuMVC.ServerSentEvents.Testing/ServerSentEventExtensionIntegratedTester.cs
@@ -1,13 +1,38 @@
 using FubuMVC.Core;
+using FubuMVC.Core.Behaviors;
 using FubuMVC.Core.Registration;
 using FubuMVC.Core.Registration.Nodes;
 using FubuMVC.Core.Registration.Routes;
 using NUnit.Framework;
 using FubuTestingSupport;
 using System.Linq;
+using StructureMap;
+using FubuMVC.StructureMap;
 
 namespace FubuMVC.ServerSentEvents.Testing
 {
+
+    [TestFixture]
+    public class Can_build_from_container
+    {
+        [Test]
+        public void end_to_end()
+        {
+            var registry = new FubuRegistry();
+            registry.Applies.ToThisAssembly();
+            registry.Import<ServerSentEventsExtension>();
+
+            var container = new Container();
+            FubuApplication.For(registry).StructureMap(container).Bootstrap();
+            var graph = container.GetInstance<BehaviorGraph>();
+            var chain = graph.BehaviorFor<ChannelWriter<FakeTopic>>(x => x.Write(null));
+
+            
+
+            container.GetInstance<IActionBehavior>(chain.UniqueId.ToString());
+        }
+    }
+
     [TestFixture]
     public class ServerSentEventExtensionIntegratedTester
     {
